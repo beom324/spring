7360d012c81890fed1ce9dff99a8d313a82c6fcb
beom324 <scarlet2010@nate.com>
2024-02-08 12:24:25 +0900

    add bookImg down

--- jsoupTest/src/main/java/com/example/demo/controller/HanBController.java
+++ jsoupTest/src/main/java/com/example/demo/controller/HanBController.java
@@ -1,4 +1,3 @@
-
 package com.example.demo.controller;
 
 import java.io.FileOutputStream;
@@ -6,7 +5,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -22,98 +23,116 @@ import com.example.demo.vo.NewBook;
 
 @RestController
 public class HanBController {
-	
+
 	@GetMapping("/downImg")
 	public String downImg() {
-		String addr="https://www.hanbit.co.kr/data/books/B7027415255_l.jpg";//이미지가 있는 URL
+		List<String> addrList = new ArrayList<>();
+		List<String> titleList = new ArrayList<>();
+		int b = 1;
 		try {
-			URL url = new URL(addr); //url객체생성
-			String fname = "퀵드로윙.jpg"; //컴퓨터에 저장 할 파일이름
-			InputStream is =url.openStream(); //스트림 얻기
-			FileOutputStream fos = new FileOutputStream("c:/data/"+fname);//출력을 할 스트림 생성
-			FileCopyUtils.copy(is.readAllBytes()	,fos);//모두 읽어와서 fos에 출력
-			
-			is.close();
-			fos.close();
-			System.out.println("이미지를 다운로드 하였습니다");
-		}catch(Exception e) {
-			System.out.println("사진다운오류"+e.getMessage());
-		}
-		
-		return "ok";
-		
-	}
-	
-		
-	@GetMapping("/newbook")	
-	public List<NewBook> newBook() {
-		List<NewBook> bookList = new ArrayList<NewBook>();
-		String base = "https://www.hanbit.co.kr";
-		//전체페이지수를 알아내서 수집하도록 코드를 수정 해 봅니다.		
-		try {
-			int i=1;
-			while(true) {		
-					String url = "https://www.hanbit.co.kr/store/books/new_book_list.html?page="+i;
-					Document doc= Jsoup.connect(url).get();
-					Elements list =  doc.select(".book_tit");
-					if(list.size() == 0) {
-						break;
-					}
-					for(Element e :list) {
-						Element a = e.firstElementChild();
-						String text = a.text();
-						String link =  base + a.attr("href");
-						bookList.add(new NewBook(text, link));
-					}
-				System.out.println(i+"페이지를 수집하였습니다.");
-				i++;
+			while (true) {
+				String url = "https://www.hanbit.co.kr/store/books/new_book_list.html?page=" + b;
+				Document doc = Jsoup.connect(url).get();
+				Elements elements = doc.select(".view_box");
+				Elements elements2 = doc.select(".book_tit");
+				if (elements.size() == 0) {
+					break;
+				}
+				for (Element e : elements) {
+					Elements img = e.getElementsByTag("img");
+					String src = img.get(img.size() - 1).attr("src");// 배열의 마지막꺼에 사진의 src가있음.
+					addrList.add(src);
+				}
+				for (Element f : elements2) {
+					Element bookname = f.firstElementChild();
+					String title = bookname.text();
+					titleList.add(title);
+				}				
+				b++;
 			}
-		}catch (Exception e) {
-			System.out.println("예외발생:"+e.getMessage());
-		}
-		return bookList;
-	}
-	
-	@GetMapping("/seat")
-	public int seat() {
-		int seat =0;
-		String url = "http://mpllc-seat.sen.go.kr/seatinfo/Seat_Info/1_count.asp";
-		try {
-			Document doc = Jsoup.connect(url).get();
-			Elements list = doc.select(".wating_f");
-			Element a = list.first();
-			seat = Integer.parseInt(a.text());
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+			for (int i = 0; i < titleList.size(); i++) {
+				
 
-		
-		return seat;
+				imgDownload("https://www.hanbit.co.kr/" + addrList.get(i), titleList.get(i));
+
+				System.out.println(titleList.get(i));
+				System.out.println(i + "번째 데이터를 저장했습니다");
+			}
+		} catch (Exception e) {
+			System.out.println("오류발생 :" + e.getMessage());
+		}
+
+		return "Ok";
 	}
-	
+
+	public void imgDownload(String addr, String fname) {
+		fname = fname.replace("/", "_");
+		fname = fname.replace("?", "_");
+		fname = fname.replace("#", "_");
+		fname = fname.replace(":", "_");
+		try {
+			URL url = new URL(addr);
+			InputStream is = url.openStream();
+			FileOutputStream fos = new FileOutputStream("c:/data/" + fname + ".jpg");
+			FileCopyUtils.copy(is.readAllBytes(), fos);
+			fos.close();
+			is.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+//	
+//
+//		
 //	@GetMapping("/newbook")	
-//	public String newBook() {
+//	public List<NewBook> newBook() {
+//		List<NewBook> bookList = new ArrayList<NewBook>();
 //		String base = "https://www.hanbit.co.kr";
-//		String url = "https://www.hanbit.co.kr/store/books/new_book_list.html";
+//		//전체페이지수를 알아내서 수집하도록 코드를 수정 해 봅니다.		
 //		try {
-//			Document doc= Jsoup.connect(url).get();
-//			Elements list =  doc.select(".book_tit");
-//			for(Element e :list) {
-//				Element a = e.firstElementChild();
-//				String text = a.text();
-//				String link =  base + a.attr("href");
-//				System.out.println(text);
-//				System.out.println(link);
-//				System.out.println("---------------------------");
+//			int i=1;
+//			while(true) {		
+//					String url = "https://www.hanbit.co.kr/store/books/new_book_list.html?page="+i;
+//					Document doc= Jsoup.connect(url).get();
+//					Elements list =  doc.select(".book_tit");
+//					if(list.size() == 0) {
+//						break;
+//					}
+//					for(Element e :list) {
+//						Element a = e.firstElementChild();
+//						String text = a.text();
+//						String link =  base + a.attr("href");
+//						bookList.add(new NewBook(text, link));
+//					}
+//				System.out.println(i+"페이지를 수집하였습니다.");
+//				i++;
 //			}
-//			
 //		}catch (Exception e) {
 //			System.out.println("예외발생:"+e.getMessage());
 //		}
-//		return "OK";
+//		return bookList;
 //	}
+//	
+//	@GetMapping("/seat")
+//	public int seat() {
+//		int seat =0;
+//		String url = "http://mpllc-seat.sen.go.kr/seatinfo/Seat_Info/1_count.asp";
+//		try {
+//			Document doc = Jsoup.connect(url).get();
+//			Elements list = doc.select(".wating_f");
+//			Element a = list.first();
+//			seat = Integer.parseInt(a.text());
+//			
+//			
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//
+//		
+//		return seat;
+//	}
+
 }
--- jsoupTest/src/main/java/com/example/demo/vo/NewBook.java
+++ jsoupTest/src/main/java/com/example/demo/vo/NewBook.java
@@ -11,4 +11,13 @@ import lombok.NoArgsConstructor;
 public class NewBook {
 	private String title;
 	private String link;
+	private String img;
+	
+	public NewBook(String title, String link) {
+		super();
+		this.title = title;
+		this.link = link;
+	}
+	
+	
 }
